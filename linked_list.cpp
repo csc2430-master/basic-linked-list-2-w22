bdca9c6e1b09931499503c920d854b323574febc
Carlos Roberto Arias Arevalo <arias@spu.edu>
2022-03-04 12:55:00 -0800

    some methods implemented

--- linked_list.cpp
+++ linked_list.cpp
@@ -11,7 +11,17 @@ using std::string;
 using std::ostream;
 
 bool LinkedList::RemoveNotInHead(const string &value) {
-    return false;
+    if (_head->data == value)
+        return false;
+    Node* itemBefore = FindItemBefore(value);
+    if (itemBefore == nullptr)
+        return false;
+    Node* toDelete = itemBefore->next;
+    itemBefore->next = toDelete->next;
+    toDelete->next = nullptr;
+    delete toDelete;
+    _size--;
+    return true;
 }
 
 LinkedList::Node *LinkedList::FindItem(const string &value) const {
@@ -35,31 +45,101 @@ LinkedList::Node *LinkedList::FindItemBefore(const string &value) const {
 }
 
 LinkedList::LinkedList() {
-
+    _head = nullptr;
+    _size = 0;
 }
 
 LinkedList::~LinkedList() {
-
+    Node* tmp = _head;
+    while (_head != nullptr){
+        _head = tmp->next;
+        tmp->next = nullptr;
+        delete tmp;
+        tmp = _head;
+    }
 }
 
 void LinkedList::Prepend(const string &value) {
+    Node* neo = new Node;
+    neo->data = value;
+    neo->next = nullptr;
 
+    if (_head == nullptr)
+        _head = neo;
+    else{
+        neo->next = _head;
+        _head = neo;
+    }
+    _size++;
 }
-
+/**
+ *
+ * @param value what to insert
+ * @param after where to insert it after
+ * @return
+ */
 bool LinkedList::InsertAfter(const string &value, const string &after) {
+    Node* item = FindItem(after);
+    if (item == nullptr)
+        return false;
+    Node* neo = new Node;
+    neo->data = value;
+    neo->next = nullptr;
+    neo->next = item->next;
+    item->next = neo;
+    _size++;
     return false;
 }
 
 bool LinkedList::RemoveItem(const string &value) {
-    return false;
+    Node* tmp = nullptr;
+    if (_head->data == value){
+        tmp = _head->next;
+        _head->next = nullptr;
+        delete _head;
+        _head = tmp;
+        _size--;
+        return true;
+    }else{
+        bool deleted = RemoveNotInHead(value);
+        if (deleted){
+            _size--;
+        }
+        return deleted;
+    }
 }
 
 bool LinkedList::Insert(size_t position, const string &value) {
-    return false;
+    if (position > _size)
+        return false;
+    Node* neo = new Node;
+    neo->data = value;
+    neo->next = nullptr;
+
+    if (_head == nullptr){
+        _head = neo;
+    }else if (position == 0){
+        Prepend(value);
+    }else{
+        Node* tmp = _head;
+        for (int i = 0; i < position - 1; ++i) {
+            tmp = tmp->next;
+        }
+        neo->next = tmp->next;
+        tmp->next = neo;
+    }
+    _size++;
+    return true;
 }
 
 bool LinkedList::Remove(size_t position, string &value) {
-    return false;
+    if (position >= _size)
+        return false;
+    Node* tmp = _head;
+    for (int i = 0; i < position - 1; ++i) {
+        tmp = tmp->next;
+    }
+
 }
 
 bool LinkedList::Get(size_t position, string &value) const {
